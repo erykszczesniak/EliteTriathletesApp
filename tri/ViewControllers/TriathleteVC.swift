69bc9a069b3d3b64542d439bebba7aba3374303f
erykszczesniak <eryks453@gmail.com>
2020-11-24 19:38:49 +0100

    organized files

--- tri/ViewControllers/TriathleteVC.swift
+++ tri/ViewControllers/TriathleteVC.swift
@@ -25,32 +25,8 @@ class TriathleteVC: UIViewController {
                    
     }
         
-    }
-
-    extension TriathleteVC: UICollectionViewDelegate, UICollectionViewDataSource {
-        
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-            return triathletes.count
-        }
-        
-        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! TriathleteCell
-            let icon = triathletes[indexPath.row]
-            cell.imageView.image = UIImage(named: icon.imageName)
-            cell.nameLabel.text = "\(icon.name)"
-            // Apply round corner
-            cell.layer.cornerRadius = 4.0
-            return cell
-        }
-        
-        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-              if segue.identifier == "showIconDetail" {
-                  if let indexPaths = collectionView?.indexPathsForSelectedItems {
-                      let destinationController = segue.destination as! DetailVC
-                      destinationController.icon = triathletes[indexPaths[0].row]
-                      collectionView?.deselectItem(at: indexPaths[0], animated: false)
-            }
-        }
-    }
 }
+
+   
+
 
